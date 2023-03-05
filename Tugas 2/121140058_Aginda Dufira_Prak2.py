229739b40cc39d4b3a375c633287d8289e55e570
Firanda11 <127003282+Firanda11@users.noreply.github.com>
2023-03-05 21:25:36 +0700

    Create 121140058_Aginda Dufira_Prak2.py

---
+++ Tugas 2/121140058_Aginda Dufira_Prak2.py
@@ -0,0 +1,14 @@
+class data:
+    def __init__(self, nama, nim, kelas_siakad, jumlah_sks):
+        self.nama = nama
+        self.nim = nim
+        self.kelas = kelas_siakad
+        self.sks = jumlah_sks
+    def tampil_data_diri(self):
+        print("Aginda Dufira")
+        print("121140058")
+        print("RB")
+        print("20")
+data = data ("Aginda Dufira", 121140058, "RB", 20)
+data.tampil_data_diri()
+   
